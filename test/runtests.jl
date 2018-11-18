a118e0443ed6ee0b2000f4a7df7ed3f61665dd98
German Vega <43078068+gava31@users.noreply.github.com>
2018-11-18 14:10:35 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,6 +18,7 @@ f(x)=-10x^3-5x^2+20
 f′(x)=-20x^2-10x
 @test newtonroot(f,f′,x₀=0.2)[1]≈1.11338619006481 atol=0.000001
 
+
 #Test method 2
 f(x)=x^2-4.0
 @test newtonroot(f,x₀=0.2)[1]≈2.0 atol=0.000001
@@ -31,8 +32,28 @@ f(x)=3x^2-5x+1
 f(x)=-10x^3-5x^2+20
 @test newtonroot(f,x₀=0.2)[1]≈1.11338619006481 atol=0.000001
 
+
 #Test non-convergence (return nothing)
 f(x)=2+x^2
 @test newtonroot(f,x₀=0.2)==nothing
+
+
+#Test maxiter (first should return the value, second should return nothing)
+f(x)=log(x)-20
+a=newtonroot(f,x₀=0.2)[1] #Algorithm needs 17 iterations in this case
+b=newtonroot(f,x₀=0.2,maxiter=5)
+@testset "maxiter" begin
+  @test a≈4.851651954097909e8 atol=0.000001 
+  @test b==nothing
+ end;
+
+
+#Test tolerance (As tolerance parameter increases, the code is less accurate)
+f(x)=3x^2-5x+1
+a=newtonroot(f,x₀=1)[1]
+b=newtonroot(f,x₀=1, tolerance=0.0005)[1]
+c=newtonroot(f,x₀=1, tolerance=0.05)[1]
+@test f(a)<f(b)<f(c)
+
 
 

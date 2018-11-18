008765c5af837e09776d930b295e66846b95e457
German Vega <43078068+gava31@users.noreply.github.com>
2018-11-18 13:23:59 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,11 @@
 using NewtonsMethod
 using Test
 
-
+#Test method 1
 f(x)=x^2-4.0
 f′(x)=2x
-@testset newtonroot(f,f′,x₀=0.2)[1]==2.0
+@test newtonroot(f,f′,x₀=0.2)[1]==2.0
+
+#Test method 2
+f(x)=3x^2-5x+1
+@test newtonroot(f,x₀=0.5)[1]≈0.23240824122077 atol=0.000001
